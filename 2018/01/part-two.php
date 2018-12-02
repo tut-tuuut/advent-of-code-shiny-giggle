71adfef38368277f6329490647975c43315980f2
Agnès H <agnes.haasser@gmail.com>
2018-12-02 18:24:36 +0100

    Second attempt on day 1 - part 2

--- 2018/01/part-two.php
+++ 2018/01/part-two.php
@@ -11,22 +11,26 @@ $frequency = 0;
 $reachedFrequencies = [];
 $reachedFrequencies[$frequency] = 'OH!';
 
-foreach ($input as $inputRow) {
-    $sign = $inputRow[0];
-    $value = (int)substr($inputRow, 1);
-    if ($sign === '+') {
-        $frequency += $value;
-    } elseif ($sign === '-') {
-        $frequency -= $value;
-    } else {
-        say('Uh???');
-    }
-    if (isset($reachedFrequencies[$frequency])) {
-        say("First frequency reached twice: " . $frequency);
-        break;
-    } else {
-        $reachedFrequencies[$frequency] = 'AH!';
+$numberOfLoops = 0;
+
+while(true) {
+    $numberOfLoops++;
+    say('Beginning loop #'.$numberOfLoops.'…');
+    foreach ($input as $inputRow) {
+        $sign = $inputRow[0];
+        $value = (int)substr($inputRow, 1);
+        if ($sign === '+') {
+            $frequency += $value;
+        } elseif ($sign === '-') {
+            $frequency -= $value;
+        } else {
+            say('Uh???');
+        }
+        if (isset($reachedFrequencies[$frequency])) {
+            say("First frequency reached twice: " . $frequency);
+            die;
+        } else {
+            $reachedFrequencies[$frequency] = 'AH!';
+        }
     }
 }
-
-say('We will have to loop over the input…');
