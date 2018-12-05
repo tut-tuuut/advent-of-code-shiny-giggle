d92e278b62eb4969c9365c7d1cf38f8fd910edda
Agnès H <agnes.haasser@gmail.com>
2018-12-05 21:10:50 +0100

    2017 d7p1

--- 2017/07/part-1.php
+++ 2017/07/part-1.php
@@ -9,3 +9,32 @@ include(__DIR__.'/input.php');
 
 // regex to get name, weight and children of programs
 $regex = '/^(?<name>\w+) \((?<weight>\d+)\) -> (?<children>.+)/';
+
+$progList = explode(PHP_EOL, $input);
+$parentProgSet = [];
+
+foreach ($progList as $line) {
+    $matches = [];
+    if (preg_match($regex, $line, $matches)) {
+        // if regex matches, it is a parent program:
+        // save it in the set
+        $parentProgSet[$matches['name']] = "oh";
+    }
+}
+
+// then, unset every program in the set which has a parent:
+// the last one will be the root
+
+foreach ($progList as $line) {
+    $matches = [];
+    if (preg_match($regex, $line, $matches)) {
+        $children = explode(', ', $matches['children']);
+        foreach ($children as $child) {
+            unset($parentProgSet[$child]);
+        }
+    }
+}
+if (count($parentProgSet) > 1) {
+    say('uh oh');
+}
+say('the root is: '.key($parentProgSet));
