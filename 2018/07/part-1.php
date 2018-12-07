c1958e11d35591bc7ee1b6caf77ae4d986d8dd32
Agnès Haasser <ahaasser@tea-ebook.com>
2018-12-07 13:38:22 +0100

    2018d07p1 Build structure of objects

--- 2018/07/part-1.php
+++ 2018/07/part-1.php
@@ -3,11 +3,55 @@
 include(__DIR__.'/../../utils.php');
 include(__DIR__.'/input.php');
 
+$input = 'Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.';
 
 $extractLetters = '/Step (\w) must be finished before step (\w) can begin./';
+$steps = [];
 foreach (explode(PHP_EOL, $input) as $row) {
     $matches = [];
     preg_match($extractLetters, $row, $matches);
     list( , $before, $after) = $matches;
-    say("$before -> $after");
+    if (!isset($steps[$before])) {
+        $steps[$before] = new Step($before);
+    }
+    if (!isset($steps[$after])) {
+        $steps[$after] = new Step($after);
+    }
+    $steps[$before]->mustBeCompletedBefore($steps[$after]);
+}
+
+class Step
+{
+    private $letter = '';
+
+    /**
+     * @var Step[]
+     */
+    private $before = [];
+
+    /**
+     * @var Step[]
+     */
+    private $after = [];
+
+    public function __construct($letter)
+    {
+        $this->letter = $letter;
+    }
+
+    public function dependsOn(Step $step)
+    {
+        $this->before[] = $step;
+    }
+
+    public function mustBeCompletedBefore(Step $step) {
+        $this->after[] = $step;
+        $step->dependsOn($this);
+    }
 }
