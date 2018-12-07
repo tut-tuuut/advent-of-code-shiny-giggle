8f5659e2f9cfc2ff1905ab282b420af8105cffa8
Agnès Haasser <ahaasser@tea-ebook.com>
2018-12-07 14:26:43 +0100

    2018 d7p1: done

--- 2018/07/part-1.php
+++ 2018/07/part-1.php
@@ -3,13 +3,13 @@
 include(__DIR__.'/../../utils.php');
 include(__DIR__.'/input.php');
 
-$input = 'Step C must be finished before step A can begin.
+/*$input = 'Step C must be finished before step A can begin.
 Step C must be finished before step F can begin.
 Step A must be finished before step B can begin.
 Step A must be finished before step D can begin.
 Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
-Step F must be finished before step E can begin.';
+Step F must be finished before step E can begin.';*/
 
 $extractLetters = '/Step (\w) must be finished before step (\w) can begin./';
 $steps = [];
@@ -99,6 +99,7 @@ class Step
     public function do()
     {
         self::$sequence .= $this->letter;
+        $this->done = true;
         say(self::$sequence);
     }
 
