26694848af8ce9f0a69ebdf58025d7ade1e43eda
Agnès H <agnes.haasser@gmail.com>
2018-12-11 22:46:12 +0100

    2018 d11p1 done2018 d11p1 done

--- 2018/11/part-1.php
+++ 2018/11/part-1.php
@@ -30,5 +30,36 @@ checkEquals(hundredsDigit(121), 1, 'for input 121');
 say('Check cellPowerLevel: ', COLOR_NICE_PINK);
 checkEquals(cellPowerLevel(3, 5, 8),  4);
 checkEquals(cellPowerLevel(122, 79, 57),  -5);
-checkEquals(cellPowerLevel(217, 169, 39),  0);
+checkEquals(cellPowerLevel(217, 196, 39),  0);
 checkEquals(cellPowerLevel(101, 153, 71),  4);
+
+// so, let's begin with the problem
+// for each point in the grid, calculate the power level of its 3x3 square
+const GRID_SIZE = 300;
+
+say('calculating power levels…');
+$powerLevels = [];
+for ($i = 1; $i <= GRID_SIZE; $i++) {
+    for ($j = 1; $j <= GRID_SIZE; $j++) {
+        $powerLevels[$j][$i] = cellPowerLevel($i, $j);
+    }
+}
+
+say('seeking for best power cell…');
+$maxPower = 0;
+$bestCoordinates = [];
+for ($i = 1; $i <= GRID_SIZE - 2; $i++) {
+    for ($j = 1; $j <= GRID_SIZE - 2; $j++) {
+        $sum = 0;
+        for ($x = $i; $x <= $i + 2; $x++) {
+            for ($y = $j; $y <= $j + 2; $y++) {
+                $sum += $powerLevels[$y][$x];
+            }
+        }
+        if ($sum > $maxPower) {
+            $maxPower = $sum;
+            $bestCoordinates = [$i, $j];
+        }
+    }
+}
+say(bashColor(COLOR_NICE_BLUE, '[PART 1] ').implode(',', $bestCoordinates));
