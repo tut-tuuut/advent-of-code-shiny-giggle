9513914a42141f9765c4883099a3760b50c69643
Agnès H <agnes.haasser@gmail.com>
2018-12-15 22:19:16 +0100

    make units decide if they move or not
    moving will be another piece of stuff to code

--- 2018/15/part-1.php
+++ 2018/15/part-1.php
@@ -8,26 +8,36 @@ $climate = new League\CLImate\CLImate;
 
 $input = file_get_contents(__DIR__.'/input-2.txt');
 
-$input = str_replace('G', '<red>G</red>', $input);
-$input = str_replace('E', '<green>E</green>', $input);
-
-$climate->clear();
-
-$climate->out($input);
-
 $grid = [];
 foreach (explode(PHP_EOL, $input) as $y => $inputrow) {
     foreach (str_split($inputrow) as $x => $char) {
         if ($char === '#' || $char === '.') {
             $grid[$y][$x] = $char;
         } elseif ($char === 'G') {
+            say('new goblin at '.$x.'-'.$y);
             $grid[$y][$x] = new Goblin($x, $y);
         } elseif ($char === 'E') {
+            say('new elf at '.$x.'-'.$y);
             $grid[$y][$x] = new Elf($x, $y);
         }
     }
 }
 draw($grid, $climate);
+$grid = makeTurn($grid);
+
+function makeTurn($grid) {
+    $activeGrid = $grid;
+    foreach ($grid as $y => $row) {
+        foreach ($row as $x => $place) {
+            if (!($place instanceof Unit)) {
+                continue;
+            }
+            $place->move($activeGrid);
+            $place->attack($activeGrid);
+        }
+    }
+    return $activeGrid;
+}
 
 function draw($grid, $cli) {
     $cli->clear();
@@ -47,11 +57,11 @@ abstract class Unit
     private $hp;
     private $attack;
 
-    public function construct($x, $y)
+    public function __construct($x, $y)
     {
         $this->x = $x;
         $this->y = $y;
-        $this->hp = 300;
+        $this->hp = 200;
         $this->attack = 3;
     }
 
@@ -69,6 +79,52 @@ abstract class Unit
     {
         return $this->type;
     }
+
+    public function isEnemy(Unit $unit)
+    {
+        return ($this->type !== $unit->getType());
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function move(&$grid)
+    {
+        foreach (
+            $this->findNeighbours($this->x, $this->y, $grid)
+            as $neighbourInfo
+        ) {
+            $neighbour = $neighbourInfo[2];
+            if ($neighbour instanceof Unit
+            && $this->isEnemy($neighbour)) {
+                say($this->type.' stop!');
+                return false;
+            }
+        }
+        say($this->type.' let’s move!');
+    }
+
+    private function findNeighbours($x, $y, &$grid)
+    {
+        foreach ([
+            [$this->y, $this->x - 1],
+            [$this->y, $this->x + 1],
+            [$this->y - 1, $this->x],
+            [$this->y + 1, $this->x],
+        ] as $neighbourCoordinates) {
+            list($y, $x) = $neighbourCoordinates;
+            if (isset($grid[$y][$x])) {
+                yield [$x, $y, $grid[$y][$x]];
+            }
+        }
+    }
+
+    public function attack(&$grid)
+    {
+    }
+
 }
 
 class Elf extends Unit
