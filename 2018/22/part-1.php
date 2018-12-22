fb696364a78a7aaa46e5b27535d352fb7aa02fd1
Agnès H <agnes.haasser@gmail.com>
2018-12-22 09:29:59 +0100

    Better algorithm, after properly reading the story

--- 2018/22/part-1.php
+++ 2018/22/part-1.php
@@ -1,4 +1,6 @@
 <?php
+
+use League\CLImate\CLImate;
 include(__DIR__.'/../../vendor/autoload.php');
 include(__DIR__.'/../../utils.php');
 
@@ -7,19 +9,29 @@ const TARGET_Y = 10;//743;
 
 const CAVE_DEPTH = 520; // 8112;
 
+$cli = new CLImate();
+
 $history = [];
 $totalRiskLevel = 0;
-for ($x = 0; $x <= TARGET_X; $x++) {
-    say($x);
-    for ($y = 0; $y <= TARGET_Y; $y++) {
-        $totalRiskLevel += getRiskLevel($x, $y, $history);
+for ($y = 0; $y <= TARGET_Y; $y++) {
+    for ($x = 0; $x <= TARGET_X; $x++) {
+        $risk = getRiskLevel($x, $y, $history);
+        $cli->inline($risk.' ');
+        $totalRiskLevel += $risk;
     }
+    $cli->out('');
 }
 say('part 1:'. $totalRiskLevel);
 
 function getRiskLevel($x, $y, &$history)
 {
-    return (getGeologicalIndex($x, $y, $history) + 20183)%3;
+    return (getErosionLevel($x, $y, $history))%3;
+}
+
+function getErosionLevel($x, $y, &$history)
+{
+    $index = getGeologicalIndex($x, $y, $history);
+    return ($index + CAVE_DEPTH) % 20183;
 }
 
 function getGeologicalIndex($x, $y, &$history)
@@ -33,13 +45,13 @@ function getGeologicalIndex($x, $y, &$history)
     } elseif ($y === TARGET_Y && $x === TARGET_X) {
         $result = 0;
     } elseif ($y === 0) {
-        $result = $x * 16807;
+        $result = ($x%20183) * (16807%20183);
     } elseif ($x === 0) {
-        $result = $y * 48271;
+        $result = ($y%20183) * (48271%20183);
     } else {
         $result = getGeologicalIndex($x - 1, $y, $history) * getGeologicalIndex($x, $y - 1, $history);
     }
-    $history[$x][$y] = $result;
+    $history[$x][$y] = $result % 20183;
     return $result;
     // The region at the coordinates of the target has a geologic index of 0.
     // If the region's Y coordinate is 0, the geologic index is its X coordinate times 16807.
