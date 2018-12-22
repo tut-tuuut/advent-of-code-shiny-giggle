b8565549d656882e6bf2a3be6b2e528af0120df3
Agnès H <agnes.haasser@gmail.com>
2018-12-22 10:02:56 +0100

    Use GMP library for manipulating huge numbers

--- 2018/22/part-1.php
+++ 2018/22/part-1.php
@@ -7,7 +7,7 @@ include(__DIR__.'/../../utils.php');
 const TARGET_X = 10;//13;
 const TARGET_Y = 10;//743;
 
-const CAVE_DEPTH = 520; // 8112;
+const CAVE_DEPTH = 510;//8112;
 
 $cli = new CLImate();
 
@@ -16,22 +16,28 @@ $totalRiskLevel = 0;
 for ($y = 0; $y <= TARGET_Y; $y++) {
     for ($x = 0; $x <= TARGET_X; $x++) {
         $risk = getRiskLevel($x, $y, $history);
-        $cli->inline($risk.' ');
+        if ($x === 0 && $y === 0) {
+            $cli->inline('M');
+        } elseif ($x === TARGET_X && $y === TARGET_Y) {
+            $cli->inline('T');
+        } else {
+            $cli->inline(draw($risk));
+        }
         $totalRiskLevel += $risk;
     }
     $cli->out('');
 }
-say('part 1:'. $totalRiskLevel);
+say('part 1: '. $totalRiskLevel);
 
 function getRiskLevel($x, $y, &$history)
 {
-    return (getErosionLevel($x, $y, $history))%3;
+    return gmp_mod(getErosionLevel($x, $y, $history), 3);
 }
 
 function getErosionLevel($x, $y, &$history)
 {
     $index = getGeologicalIndex($x, $y, $history);
-    return ($index + CAVE_DEPTH) % 20183;
+    return gmp_mod($index + CAVE_DEPTH, 20183);
 }
 
 function getGeologicalIndex($x, $y, &$history)
@@ -45,16 +51,28 @@ function getGeologicalIndex($x, $y, &$history)
     } elseif ($y === TARGET_Y && $x === TARGET_X) {
         $result = 0;
     } elseif ($y === 0) {
-        $result = ($x%20183) * (16807%20183);
+        $result = gmp_mul($x, 16807);
     } elseif ($x === 0) {
-        $result = ($y%20183) * (48271%20183);
+        $result = gmp_mul($y, 48271);
     } else {
         $result = getGeologicalIndex($x - 1, $y, $history) * getGeologicalIndex($x, $y - 1, $history);
     }
-    $history[$x][$y] = $result % 20183;
+    $history[$x][$y] = $result;
     return $result;
     // The region at the coordinates of the target has a geologic index of 0.
     // If the region's Y coordinate is 0, the geologic index is its X coordinate times 16807.
     // If the region's X coordinate is 0, the geologic index is its Y coordinate times 48271.
     // Otherwise, the region's geologic index is the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
+}
+
+function draw($value)
+{
+    if ($value == 0) {
+        return '.';
+    } elseif ($value == 1) {
+        return '=';
+    } elseif ($value == 2) {
+        return '|';
+    }
+    return '#';
 }
