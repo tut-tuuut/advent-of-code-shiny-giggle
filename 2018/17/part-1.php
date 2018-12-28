d6110818e40eecf908eb6a0da626599a7d1f1235
Agnès H <agnes.haasser@gmail.com>
2018-12-28 12:19:50 +0100

    Trash almost everything

--- 2018/17/part-1.php
+++ 2018/17/part-1.php
@@ -77,117 +77,7 @@ $grid[0][500] = SOURCE; // water source
 foreach ($clayPoints as $p) {
     $grid[$p['y']][$p['x']] = CLAY;
 }
-//draw($grid);
-// Make water flow --------------------------------------------------------------------------------
-$sources = [[0,500]]; // from which water falls
-while(count($sources)) {
-    $newSources = [];
-    foreach ($sources as $source) {
-        $newSources = array_merge($newSources, waterFlows($source, $grid));
-    }
-    $sources = $newSources;
-}
 draw($grid);
-// When we have no sources anymore, we can begin to count wet cells -------------------------------
-$wetCells = 0;
-foreach ($grid as $y => $row) {
-    $wetCells += count(array_filter($row, 'isWet'));
-}
-say("$wetCells in the grid!");
-
-// ------------------------------------------------------------------------------------------------
-function isWet($cell)
-{
-    return $cell === FREE_WATER || $cell === RESTING_WATER;
-}
-
-function isCrossable($cell)
-{
-    return $cell === SAND || $cell == FREE_WATER;
-}
-
-function waterFlows($source, &$grid)
-{
-    list($sy, $sx) = $source;
-    $newSources = [];
-    $depth = 0;
-    $drill = true;
-    $newWetCells = [];
-    while ($drill) {
-        $depth += 1;
-        if (!isset($grid[$sy + $depth])) {
-            $drill = false;
-            continue;
-        }
-        if (isCrossable($grid[$sy + $depth][$sx])) {
-            $grid[$sy + $depth][$sx] = FREE_WATER;
-            $newWetCells[] = [$sy + $depth, $sx];
-            $drill = true;
-        } else {
-            $drill = false;
-        }
-    }
-
-    foreach(array_reverse($newWetCells) as $cell) {
-        list($closed, $restWaterCandidates) = getSupportedNeighbours($cell, $grid);
-        // check if it can be transformed in resting water: surrounded by two # and above # or ~
-        if ($closed) {
-            foreach ($restWaterCandidates as $yx) {
-                list($y, $x) = $yx;
-                $grid[$y][$x] = RESTING_WATER;
-            }
-        } else {
-            foreach ($restWaterCandidates as $yx) {
-                list($y, $x) = $yx;
-                $grid[$y][$x] = FREE_WATER;
-                $newSources[] = [$y, $x];
-            }
-        }
-    }
-    return $newSources;
-}
-
-function getSupportedNeighbours($cell, &$grid)
-{
-    list($sy, $sx) = $cell;
-    $isClosedOnBothSides = true;
-    $restWaterCandidates = [];
-    foreach ([LEFT, RIGHT] as $direction) {
-        $flowToDirection = true;
-        $toDirection = 0;
-        $isClosedOnThisSide = false;
-        while ($flowToDirection) {
-            $toDirection += 1;
-            if (!isset($grid[$sy][$sx + $direction * $toDirection])) {
-                $flowToDirection = false;
-                continue;
-            }
-            if (isCrossable($grid[$sy][$sx + $direction * $toDirection])
-            && !isCrossable(f($grid, $sy + 1, $sx + $direction * $toDirection))) {
-                // on a clay or resting water surface
-                //      ->   X
-                //       #####
-                $restWaterCandidates[] = [$sy, $sx + $direction * $toDirection];
-            } elseif (!isCrossable($grid[$sy][$sx + $direction * $toDirection])) {
-                // bumping on the wall of a container
-                //   -> X#
-                //      #
-                $isClosedOnThisSide = true;
-                $flowToDirection = false;
-            } elseif (isCrossable($grid[$sy][$sx + $direction * $toDirection]) // a
-            && isCrossable(f($grid, $sy + 1, $sx + $direction * $toDirection)) // b
-            && !isCrossable(f($grid, $sy + 1, $sx + $direction * ($toDirection - 1))) ) { // c
-                // no side wall for current container on this direction
-                // -> a
-                // ##cb
-                $restWaterCandidates[] = [$sy, $sx + $direction * $toDirection];
-                $flowToDirection = false;
-            }
-        }
-        $isClosedOnBothSides = $isClosedOnBothSides && $isClosedOnThisSide;
-    }
-    return [$isClosedOnBothSides, $restWaterCandidates];
-}
 
 // Draw map for debug ---------------------------------------------------------------------------
 function draw(&$grid, $clear = true)
@@ -202,9 +92,4 @@ function draw(&$grid, $clear = true)
         $str = str_replace('~', '<light_blue>~</light_blue>', $str);
         $cli->out($str);
     }
-}
-
-// find value of cell on row y and col x, without yielding a notice if its unset
-function f(&$grid, $y, $x, $default = SAND) {
-    return isset($grid[$y][$x]) ? $grid[$y][$x] : $default;
 }
