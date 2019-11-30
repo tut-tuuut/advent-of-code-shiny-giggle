b3b04f181230ef10627c1da71a044e209df624cc
Agnès H <agnes.haasser@gmail.com>
2019-11-30 23:32:23 +0100

    2015-03-b solution

--- 2015/03/code.py
+++ 2015/03/code.py
@@ -34,3 +34,27 @@ with open(__file__+'.input.txt', "r+") as file:
     input = file.read()
 
 print(f"with the drunken elf, santa will visit {how_many_visited_houses(input)} houses!")
+
+def with_robosanta(instructions):
+    visited = {'0:0' : True}
+    x,y,nx,ny = 0,0,0,0
+    for step in instructions:
+        if step == '>':
+            x += 1
+        elif step == '<':
+            x -= 1
+        elif step == 'v':
+            y -= 1
+        elif step == '^':
+            y += 1
+        else:
+            next
+        visited[key(x,y)] = True
+        x,y,nx,ny = nx,ny,x,y # !
+    return len(visited)
+
+print(f"this should be 3: {with_robosanta('^v')}")
+print(f"this should be 3: {with_robosanta('^>v<')}")
+print(f"this should be 11: {with_robosanta('^v^v^v^v^v')}")
+
+print(f"With robosanta, santa will visit {with_robosanta(input)} houses!")
