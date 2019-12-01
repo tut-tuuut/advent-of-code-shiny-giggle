a21da530d75157b1ae69a20e2e9df325573ad2cc
Agnès H <agnes.haasser@gmail.com>
2019-12-01 12:06:38 +0100

    2015-04-a

---
+++ 2015/04/code.py
@@ -0,0 +1,27 @@
+import hashlib
+import time
+
+input = 'iwrupvqb'
+
+def md5(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+def find_lowest_match(prefix):
+    i = 0
+    while True:
+        i += 1
+        if md5(f'{prefix}{i}')[0:5] == '00000':
+            return i
+
+print(find_lowest_match(input))
+
+def find_six_zeroes(prefix):
+    i = 0
+    while True:
+        i += 1
+        if i % 1000000 == 0:
+            print(f'{time.ctime()} i = {i}')
+        if md5(f'{prefix}{i}')[0:5] == '000000':
+            return i
+
+print(find_six_zeroes(input))
