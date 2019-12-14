a9724dd6a2aa8ca299399f1f7c9d3f20b38e2f0f
Agnès H <agnes.haasser@gmail.com>
2019-12-14 22:52:38 +0100

    2019-14 part2 (yay dichotomy)

--- 2019/14.py
+++ 2019/14.py
@@ -11,6 +11,10 @@ class Nanofactory:
     def __init__(self):
         self.quantities = {'ORE':0}
         self.exctractedOre = 0
+    def reinit(self):
+        self.exctractedOre = 0
+        for k in self.quantities.keys():
+            self.quantities[k] = 0
     def parseRecipes(self, string):
         self.recipes = {}
         rows = string.split('\n')
@@ -36,63 +40,96 @@ class Nanofactory:
             self.use(multiplier * reactive.qty, reactive.name)
         self.quantities[name] += reaction.output.qty * multiplier
         return
+    def necessaryOreForXFuel(self, fuelQty):
+        self.reinit()
+        self.make(fuelQty, 'FUEL', 0)
+        return self.exctractedOre
 
-f = Nanofactory()
-with open(__file__ + '.t1-31') as file:
-    string1 = file.read()
+def part1():
+    f = Nanofactory()
+    with open(__file__ + '.t1-31') as file:
+        string1 = file.read()
 
-f.parseRecipes(string1)
-f.make(1, 'FUEL',0)
-print(f'this should be 31: {f.exctractedOre}')
+    f.parseRecipes(string1)
+    f.make(1, 'FUEL',0)
+    print(f'this should be 31: {f.exctractedOre}')
 
-f = Nanofactory()
-with open(__file__ + '.t3-165') as file:
-    string2 = file.read()
+    f = Nanofactory()
+    with open(__file__ + '.t3-165') as file:
+        string2 = file.read()
 
-f.parseRecipes(string2)
-f.make(1, 'FUEL', 0)
-print(f'this should be 165: {f.exctractedOre}') # 260483 too low
-print(f.quantities['ORE'])
+    f.parseRecipes(string2)
+    f.make(1, 'FUEL', 0)
+    print(f'this should be 165: {f.exctractedOre}') # 260483 too low
+    print(f.quantities['ORE'])
 
-f = Nanofactory()
-with open(__file__ + '.t2-13312') as file:
-    string2 = file.read()
-f.parseRecipes(string2)
-f.make(1, 'FUEL', 0)
-print(f'this should be 13312: {f.exctractedOre}')
+    f = Nanofactory()
+    with open(__file__ + '.t2-13312') as file:
+        string2 = file.read()
+    f.parseRecipes(string2)
+    f.make(1, 'FUEL', 0)
+    print(f'this should be 13312: {f.exctractedOre}')
 
-print('REAL INPUT ---!')
-f = Nanofactory()
+    print('REAL INPUT ---!')
+    f = Nanofactory()
+    with open(__file__ + '.input') as file:
+        string2 = file.read()
+    f.parseRecipes(string2)
+    f.make(1, 'FUEL', 0)
+    print(f'this should be more than 260483 and less than 453838: {f.exctractedOre}')
+
+#part1()
+
+def dumbPart2():
+    f = Nanofactory()
+    with open(__file__ + '.t2-13312') as file:
+        string2 = file.read()
+    f.parseRecipes(string2)
+    f.make(1, 'FUEL', 0)
+    oreForOneFuel = f.exctractedOre
+    fuelqty = 1
+    oreReserve = 1000000000000
+    minimumFeasibleFuel = int(oreReserve/oreForOneFuel)
+    f.make(minimumFeasibleFuel, 'FUEL', 0)
+    f.use(minimumFeasibleFuel, 'FUEL')
+    fuelqty += minimumFeasibleFuel
+    print(f'extracted {f.exctractedOre} to produce {fuelqty}')
+    while oreReserve > f.exctractedOre:
+        percentage = int(100*f.exctractedOre/oreReserve)
+        if percentage < 97:
+            fueltomake = 320
+        elif percentage < 99:
+            fueltomake = 200
+        else:
+            fueltomake = 1
+        f.make(fueltomake, 'FUEL', 0)
+        f.use(fueltomake, 'FUEL')
+        fuelqty += fueltomake
+        print(f'fuel qty : {fuelqty} extracted ore: {f.exctractedOre} ({percentage}%)', end='\r')
+    print('')
+    print(f'this should be 82892753: {fuelqty}')
+
+def part2(inputStr):
+    f = Nanofactory()
+    f.parseRecipes(inputStr)
+    oreForOneFuel = f.necessaryOreForXFuel(1)
+    print(f'this should be 13312: {oreForOneFuel}')
+    oreReserve = 1000000000000
+    minimumFeasibleFuel = int(oreReserve/oreForOneFuel)
+    # find by dichotomy
+    low = minimumFeasibleFuel
+    high = minimumFeasibleFuel * 2
+    pointer = int((low + high) / 2)
+    print(f'lets analyze fuel qties between {low} and {high}')
+    print(f'starting at {pointer}')
+    while high - low > 1:
+        if f.necessaryOreForXFuel(pointer) <= oreReserve:
+            low = pointer
+        else:
+            high = pointer
+        pointer = int((low + high) / 2)
+    print(f'with THAT MUCH ORE you can make {low} fuel')
+
 with open(__file__ + '.input') as file:
     string2 = file.read()
-f.parseRecipes(string2)
-f.make(1, 'FUEL', 0)
-print(f'this should be more than 260483 and less than 453838: {f.exctractedOre}')
-
-f = Nanofactory()
-with open(__file__ + '.t2-13312') as file:
-    string2 = file.read()
-f.parseRecipes(string2)
-f.make(1, 'FUEL', 0)
-oreForOneFuel = f.exctractedOre
-fuelqty = 1
-oreReserve = 1000000000000
-minimumFeasibleFuel = int(oreReserve/oreForOneFuel)
-f.make(minimumFeasibleFuel, 'FUEL', 0)
-f.use(minimumFeasibleFuel, 'FUEL')
-fuelqty += minimumFeasibleFuel
-print(f'extracted {f.exctractedOre} to produce {fuelqty}')
-while oreReserve > f.exctractedOre:
-    percentage = int(100*f.exctractedOre/oreReserve)
-    if percentage < 97:
-        fueltomake = 320
-    elif percentage < 99:
-        fueltomake = 200
-    else:
-        fueltomake = 1
-    f.make(fueltomake, 'FUEL', 0)
-    f.use(fueltomake, 'FUEL')
-    fuelqty += fueltomake
-    print(f'fuel qty : {fuelqty} extracted ore: {f.exctractedOre} ({percentage}%)', end='\r')
-print('')
-print(f'this should be 82892753: {fuelqty}')
+    part2(string2)
