8c30c729dfe85730fa8f20fd81499837d28329a1
Agnès H <agnes.haasser@gmail.com>
2019-12-24 14:05:10 +0100

    2019-24 example case OK

--- 2019/24.py
+++ 2019/24.py
@@ -12,11 +12,37 @@ exampleInput = """....#
 ..#..
 #...."""
 
-def nextGrid(strGrid):
-    strGrid = strGrid.replace('.','0').replace('#','1')
-    grid = list(map(list,strGrid.split('\n')))
-    newGrid = grid.copy()
+def nextGrid(grid):
+    newGrid = list(map(lambda row: row.copy(), grid)) # grid.copy() is not enough
     for i,j in it.product(range(5), repeat=2):
-        print(f'{i} {j} {grid[i][j]}')
+        adjacentBugs = 0
+        if i >= 1:
+            adjacentBugs += grid[i-1][j]
+        if i <= 3:
+            adjacentBugs += grid[i+1][j]
+        if j >= 1:
+            adjacentBugs += grid[i][j-1]
+        if j <= 3:
+            adjacentBugs += grid[i][j+1]
+        if grid[i][j] == 1 and adjacentBugs != 1:
+            newGrid[i][j] = 0
+        if grid[i][j] == 0 and adjacentBugs in (1,2):
+            newGrid[i][j] = 1
+    return newGrid
 
-nextGrid(exampleInput)
+def printGrid(intGrid):
+    for listRow in intGrid:
+        print(''.join(list(map(str, listRow))).replace('1','#').replace('0','.'))
+    print('\n\n')
+
+strGrid = exampleInput.replace('.','0').replace('#','1')
+intGrid = list(map(lambda strRow: list(map(int,list(strRow))),strGrid.split('\n')))
+printGrid(intGrid)
+nextGr = nextGrid(intGrid)
+printGrid(nextGr)
+nextGr = nextGrid(nextGr)
+printGrid(nextGr)
+nextGr = nextGrid(nextGr)
+printGrid(nextGr)
+nextGr = nextGrid(nextGr)
+printGrid(nextGr)
