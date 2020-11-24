a9cf8528006e1a7177f12134a66db6fa3f3f4b47
Agnès Haasser <agnes.haasser@gmail.com>
2020-11-24 10:31:28 +0100

    2015-12 part2

--- 2015/12/code.py
+++ 2015/12/code.py
@@ -9,34 +9,32 @@ def sumOfAllNumbers(string):
     regex = r'-?\d+'
     return sum(map(int, re.findall(regex, string)))
 
-def sumWithoutRedObjects(string):
-    obj = json.loads(string)
-    print(obj)
-    removeRedObjects(obj)
-    print(obj)
 
-def removeRedObjects(thing):
-    thingType = type(thing).__name__
-    if thingType == 'list':
-        print(thing)
-        thing = list(filter(shouldNotBeRemoved, thing))
-        print(thing)
-    elif thingType == 'dict':
-        for key in thing:
-            if shouldNotBeRemoved(thing[key]):
-                del(thing[key])
+def removeRedObjects(s):
+    objectsWithoutChildren = r'\{[^\{\}]*\}'
+    # \{        an opening moustache,
+    # [^\{\}]*  some characters which are not moustaches,
+    # \}        a closing moustache
+    redObjects = r'\{[^\{\}]*\:"red"[^\{\}]*\}'
+    # \{        an opening moustache,
+    # [^\{\}]*  some characters which are not moustaches,
+    # \:"red"   the string :"red",
+    # [^\{\}]*  some non-moustache characters again
+    # \}        a closing moustache
 
-def shouldNotBeRemoved(thing):
-    thingType = type(thing).__name__
-    print(thing)
-    if thingType == 'dict':
-        for key in thing:
-            if thing[key] == 'red':
-                print('i should be remvoed')
-                return False
-        return True
+    i = 0
+    while len(re.findall(objectsWithoutChildren, s)) > 0:
+        i = i+1
+        print(f"iteration {i} length {len(s)}")
+        while len(re.findall(redObjects, s)) > 0:
+            print(f'remove {len(re.findall(redObjects, s))} red objects')
+            s = re.sub(redObjects, '> 0 <', s)
+        print(f'factorize {len(re.findall(objectsWithoutChildren, s))} non red objects')
+        s = re.sub(objectsWithoutChildren, lambda x: f'> {sumOfAllNumbers(x.group(0))} <', s)
+        print('--------')
 
-
+    #print(string)
+    return s
 
 # [1,2,3] and {"a":2,"b":4} both have a sum of 6
 u.assert_equals(sumOfAllNumbers('[1,2,3]'), 6)
@@ -56,7 +54,13 @@ u.assert_equals(sumOfAllNumbers(r'{}'), 0)
 
 u.answer_part_1(sumOfAllNumbers(inputStr))
 
-sumWithoutRedObjects('[1,{"c":"red","b":2},3]')
+u.assert_equals(4, sumOfAllNumbers(removeRedObjects('[1,{"c":"red","b":2},3]')))
+u.assert_equals(sumOfAllNumbers(removeRedObjects('{"d":"red","e":[1,2,3,4],"f":5}')), 0)
+
+inputStrWithoutRed = removeRedObjects(inputStr)
+u.answer_part_2(sumOfAllNumbers(inputStrWithoutRed))
+# part2 85717 too high
+# part2 68466 yay!
 
 # Ignore any object (and all of its children) which has any property with the value "red".
 # Do this only for objects ({...}), not arrays ([...]).
