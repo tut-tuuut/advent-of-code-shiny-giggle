e4bf02f595265e72b5a275b74c55447a1f8dc315
Agnès Haasser <agnes.haasser@gmail.com>
2020-11-29 20:06:01 +0100

    2015-17 Part 1 OK before refactoring

--- 2015/17/code.py
+++ 2015/17/code.py
@@ -1,29 +1,46 @@
+import itertools
 import utils as u
 
 
-def find_all_possibilities(containers, target, possibilities=False):
-    should_return_value = False
-    if not possibilities:
-        possibilities = set()
-        should_return_value = True
-    if sum(containers) < target:
-        return 0
+def find_all_possibilities(containers, target):
     if sum(containers) == target:
-        possibilities.append(containers)
         return 1
-    if sum(containers) > target:
-        (
-            find_all_possibilities(
-                containers[:i] + containers[i + 1 :],
-                target,
-                possibilities=possibilities,
-            )
-            for i in range(len(containers))
-        )
-    if should_return_value:
-        return len(possibilities)
+    result = 0
+    for i in range(len(containers) - 1):
+        for combination in itertools.combinations(containers, i):
+            if sum(combination) == target:
+                result += 1
+    return result
 
 
 exampleContainers = (20, 15, 10, 5, 5)
 
+
+inputContainers = (
+    33,
+    14,
+    18,
+    20,
+    45,
+    35,
+    16,
+    35,
+    1,
+    13,
+    18,
+    13,
+    50,
+    44,
+    48,
+    6,
+    24,
+    41,
+    30,
+    42,
+)
+
+
 u.assert_equals(find_all_possibilities(exampleContainers, 25), 4)
+u.assert_equals(find_all_possibilities(inputContainers, 150), 1304)
+
+u.answer_part_1(find_all_possibilities(inputContainers, 150))
