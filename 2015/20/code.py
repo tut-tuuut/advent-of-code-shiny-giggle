75a959b5e66063dec42df1f0a00882a756d616c7
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-01 13:57:16 +0100

    2015 day 20 : does not work

--- 2015/20/code.py
+++ 2015/20/code.py
@@ -6,14 +6,42 @@ import scipy.stats as stats
 import utils as u
 
 # Easy part : calculate number of gifts from house number
+prime_numbers = [2, 3, 5, 7, 11]
+
+
+def yield_prime_numbers_before(target):
+    max_known_prime_number = max(prime_numbers)
+    if target < max_known_prime_number:
+        yield from (i for i in prime_numbers if i <= target)
+    if target > max_known_prime_number:
+        yield from prime_numbers
+        for i in range(max_known_prime_number, target):
+            prime = True
+            for j in prime_numbers:
+                if i % j == 0:
+                    prime = False
+                    break
+            if prime == True:
+                prime_numbers.append(i)
+                yield i
+
+
+def get_prime_divisors(number):
+    for prime_divisor in yield_prime_numbers_before(number):
+        divisor = prime_divisor
+        while number % divisor == 0:
+            yield prime_divisor
+            divisor *= prime_divisor
 
 
 def get_number_of_gifts(house_number):
     result = 0
-    return sum(
-        10 * elf for elf in range(1, house_number + 1) if house_number % elf == 0
+    return 10 * sum(
+        elf for elf in range(1, house_number + 1) if house_number % elf == 0
     )
 
+
+print(list(get_prime_divisors(725)))
 
 # it works but it's an O(n)
 
