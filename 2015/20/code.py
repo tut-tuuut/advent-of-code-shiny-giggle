1c5f49ef43efd6ba744fe82e79930e7d33c17baa
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-04 23:05:17 +0100

    2015-20 part 1

--- 2015/20/code.py
+++ 2015/20/code.py
@@ -1,4 +1,5 @@
 import math
+import time
 
 import numpy
 import scipy.stats as stats
@@ -7,74 +8,62 @@ import utils as u
 
 # Easy part : calculate number of gifts from house number
 
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
 
 def get_number_of_gifts(house_number):
     result = 0
-    return 10 * sum(
-        elf for elf in range(1, house_number + 1) if house_number % elf == 0
-    )
+    return 10 * sigma(house_number)
 
 
-sigmas = {}
+# store a little bunch of sigmas: for every prime number under 100
+# sigma(x) = x + 1 (a prime number only divides itself and 1)
+sigmas = {prime: prime + 1 for prime in u.PRIME_NUMBERS}
 
 
 def sigma(number):
     """sum of divisors of number"""
+    # smartness #1: if we know it already, return it
     if number in sigmas:
-        return sigmas(number)
-    sigma = sum(divisor for divisor in range(1, number + 1) if number % divisor == 0)
-    sigmas[number] = sigma
-    return sigma
+        # print(f"smartness # we know already sigma({number})")
+        return sigmas[number]
 
+    # preparation for smartness #3: prime numbers are nice for sigma
+    is_probably_prime = True
 
-u.assert_equals(sigma(10), 18)
-print(sigma(5))
+    # smartness #2: sigma is a multiplicative function:
+    # if math.gcd(x,y) == 1 then sigma(x * y) = sigma(x) * sigma(y)
+    # so we try to use memoization to avoid a huge loop when possible
+    for x in range(2, number // 2):
+        # if x does not divide number, continue:
+        if number % x != 0:
+            continue
 
-# it works but it's an O(n)
+        # if we are here, we found a divider to number, it's not a prime
+        is_probably_prime = False
 
+        y = number // x
+        # if we cannot use the multiplicativeness of sigma, continue:
+        if math.gcd(x, y) > 1:
+            continue
+        # if we are here, great!
+        # print(f"hey! sigma({number}) = sigma({x})sigma({y})")
+        sigmas[number] = sigma(x) * sigma(y)
+        return sigmas[number]
 
-# house 10:
-# elf 1, 2, 5, 10 come by, result = 10*(1 + 2 + 5 + 10) = 10 * 18
+    # smartness 3: if we did not find any divider to number,
+    # calculate sigma as if it was a prime number. It is very difficult,
+    # as you can see:
+    if is_probably_prime:
+        sigmas[number] = number + 1
+        return sigmas[number]
 
-# house 9:
-# elf 1, 3, 9 come by, result = 10*(1+3+9) = 10 * 13
+    # no smartness worked, just sum all the dividers already
+    sigmas[number] = sum(
+        divisor for divisor in range(1, number + 1) if number % divisor == 0
+    )
+    return sigmas[number]
 
-# min value of gifts for house N  = 10 + N for houses which are prime numbers
-
-# house 20:
-# elf 1, 2, 4, 5, 10, 20 stop by
-# prime factors 2,2,5 => elves 2 and 5
-# not prime factors 4, 10 : combinations of prime factors 2*2 and 2*5
-
-# house 100:
-# elves 1, 2, 4, 5, 10, 20, 50
-# prime factors 5 5 2 2
-# snap, not a good example
-
-# house 16
-# elves 1, 2, 4, 8, 16
-# prime factors 2 2 2 2
-# combinations 2*2=4 2*2*2=8
-
-# house 6
-# elves 1, 2, 3, 6
-# prime factors 2 3
-# combinations others than 6: none
-
-# house 8
-# elves 1, 2, 4, 8
-# prime factors 2 2 2
-# combinations : 2*2
-
-#
-# so maybe we can make a get_number_of_gifts which is not a O(n) ?
-# with a result which could be
-#      10 * (1 + N)
-#      + something with unique prime factors < N
-#      + something with combinations of prime factors with product < N
-
-# house 8:
-# elf 1, 2, 4,
 
 u.assert_equals(get_number_of_gifts(1), 10)
 u.assert_equals(get_number_of_gifts(2), 30)
@@ -85,53 +74,24 @@ u.assert_equals(get_number_of_gifts(6), 120)
 u.assert_equals(get_number_of_gifts(7), 80)
 u.assert_equals(get_number_of_gifts(8), 150)
 u.assert_equals(get_number_of_gifts(9), 130)
-u.assert_equals(get_number_of_gifts(10), 180)
 
-# Hard part : find the house which gets 36 million gifts.
-target = 36000000
+TARGET = 36000000
+top_gifts = 0
+house_number = 0
+init_time = time.time()
 
-# When you look at the graph, you see that some houses
-# are over privileged, and their gift count seems to follow a line:
-# try to calculate the values of this line to search around where the line
-# reaches 36M.
-"""
-max_gifts = 0
-houses = []
-gift_counts = []
-print("calculating the over privileged houses...")
-for house in range(1000, 10000):
-    if house % 1000 == 0:
-        print(".", end="")
-    nb_gifts = get_number_of_gifts(house)
-    if nb_gifts > max_gifts:
-        if max_gifts > 0:
-            houses.append(house)
-            gift_counts.append(nb_gifts)
-        max_gifts = nb_gifts
-print(" done!")
-x = numpy.array(houses)
-y = numpy.array(gift_counts)
-"""
-# the line has the equation a * nb_house + b = nb_gift !
-# so I could look at house number (36M - b) / a
-"""
-a, b, _, _, _ = stats.linregress(x, y)
-starting_house = int((target - b) / a)
-print(f"a = {a} b = {b}")
-print("looking for the house with 36M gifts...")
-"""
-max_gifts = 0
-for house in range(50000, 100000):
-    if house % 1000 == 0:
-        print("house", house)
-    nb_gifts = get_number_of_gifts(house)
-    if nb_gifts > max_gifts:
-        max_gifts = nb_gifts
-        print(f"house {house} : {nb_gifts} gifts")
-        if nb_gifts >= target:
-            u.answer_part_1(house)
+for i in range(2, 1000000):
+    if i % 1000 == 0:
+        print(f"------ {i} - {time.time() - init_time} -------")
+    nb_of_gifts = get_number_of_gifts(i)
+    if nb_of_gifts > top_gifts:
+        top_gifts = nb_of_gifts
+        house_number = i
+        print(f"{nb_of_gifts} gifts in house {i}")
+        if nb_of_gifts > TARGET:
+            u.answer_part_1(i)
+            break
 
-# 971880 too high????? duh
-# 970200 too high too… noes
-
-# so I will start at 971879 and go down until I find another good one
+# 36902400 gifts in house 831600
+# [PART 1] 831600
+# the code need to run for 883 seconds for that ><
--- 2015/20/utils.py
+++ 2015/20/utils.py
@@ -1,11 +1,77 @@
-def assert_equals(tested, expected, comment = ''):
+def assert_equals(tested, expected, comment=""):
     if tested == expected:
-        print(f'\033[92m(YAY)\033[0m {tested} {comment}')
+        print(f"\033[92m(YAY)\033[0m {tested} {comment}")
     else:
-        print(f'\033[91m(NAY)\033[0m got {tested}, expected {expected}! {comment}')
+        print(f"\033[91m(NAY)\033[0m got {tested}, expected {expected}! {comment}")
+
 
 def answer_part_1(content):
-    print(f'\033[93m[PART 1] {content}\033[0m')
+    print(f"\033[93m[PART 1] {content}\033[0m")
+
 
 def answer_part_2(content):
-    print(f'\033[93m[PART 2] {content}\033[0m')
+    print(f"\033[93m[PART 2] {content}\033[0m")
+
+
+PRIME_NUMBERS = (
+    2,
+    3,
+    5,
+    7,
+    11,
+    13,
+    17,
+    19,
+    23,
+    29,
+    31,
+    37,
+    41,
+    43,
+    47,
+    53,
+    59,
+    61,
+    67,
+    71,
+    73,
+    79,
+    83,
+    89,
+    97,
+    101,
+    179,
+    103,
+    181,
+    107,
+    191,
+    109,
+    193,
+    113,
+    197,
+    127,
+    199,
+    131,
+    211,
+    137,
+    223,
+    139,
+    227,
+    149,
+    229,
+    151,
+    233,
+    157,
+    239,
+    163,
+    241,
+    167,
+    251,
+    173,
+    257,
+    263,
+    269,
+    271,
+    277,
+    281,
+)
