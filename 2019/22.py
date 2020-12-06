b753b5df736ed70bddbd274cc45691263ed1d8b6
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-06 22:33:35 +0100

    2019 22 part 2 - research

--- 2019/22.py
+++ 2019/22.py
@@ -13,7 +13,7 @@ def cut(deck, number_of_cards):
     return deck[number_of_cards:] + deck[:number_of_cards]
 
 
-def deal_with_increment(deck, increment):
+def pick_one_in_increment(deck, increment):
     # First try: does not work exactly how it should
     # (= does not work)
     # 012301230123
@@ -118,3 +118,72 @@ for instruction in raw_input.splitlines():
 # ↑↑↑ WRONG! They asked the position of card 2019
 
 print(f"PART ONE : {deck.index(2019)}")
+
+# part 2 ----------------------------------------
+
+print("-------")
+
+
+def get_previous_index_of_card(index, deck_length, instruction):
+    if "deal into" in instruction:
+        return deck_length - index - 1  # check
+    else:
+        number = int(digit_pattern.search(instruction).group())
+        if "deal" in instruction:
+            # i don't know why it works
+            # https://twitter.com/tut_tuuut/status/1335683048264884230
+            return (
+                1 + index // number + number * (number - index % number)
+            ) % deck_length
+        elif "cut" in instruction:
+            return (index + number) % deck_length
+        else:
+            print(f"Unknown instruction: {instruction}")
+            return deck
+
+
+# result 0 3 6 9 2 5 8 1 4 7
+index = 9
+for instr in reversed(
+    """deal with increment 7
+deal into new stack
+deal into new stack""".splitlines()
+):
+    index = get_previous_index_of_card(index, 10, instr)
+print("should be 7:", index)
+print("------")
+
+
+# result 3 0 7 4 1 8 5 2 9 6
+index = 8
+for instr in reversed(
+    """cut 6
+deal with increment 7
+deal into new stack""".splitlines()
+):
+    index = get_previous_index_of_card(index, 10, instr)
+print("should be 9:", index)
+print("------")
+
+# result 6 3 0 7 4 1 8 5 2 9
+index = 0
+for instr in reversed(
+    """deal with increment 7
+deal with increment 9
+cut -2""".splitlines()
+):
+    print(index)
+    index = get_previous_index_of_card(index, 10, instr)
+print("should be 6:", index)
+print("------")
+
+
+index = 2020
+deck_length = 119315717514047
+instructions = list(raw_input.splitlines())
+instructions.reverse()
+for instruction in instructions:
+    index = get_previous_index_of_card(index, deck_length, instruction)
+    if index == 2020:
+        print("aha!")
+print(index)
