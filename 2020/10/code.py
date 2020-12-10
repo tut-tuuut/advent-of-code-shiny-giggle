a82fa0d87c39b97aa6b6016abd97bac9aeec679e
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-10 21:51:01 +0100

    2020 day 10 ⭐️⭐️

--- 2020/10/code.py
+++ 2020/10/code.py
@@ -1,3 +1,4 @@
+import math
 import utils as u
 
 with open(__file__ + ".input.txt", "r+") as file:
@@ -65,3 +66,33 @@ u.assert_equals(plug_all_adapters_in_charging_outlet(second_example_adapters), 2
 u.answer_part_1(plug_all_adapters_in_charging_outlet(raw_input))
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def find_all_combinations_of_adapters(raw_list_of_adapters_in_my_bag):
+    joltages = list(map(int, raw_list_of_adapters_in_my_bag.splitlines()))
+    joltages.append(0)  # the outlet
+    joltages.append(max(joltages) + 3)  # the target
+    joltages.sort()  # "make the chain"
+    groups = []
+    new_group = []
+    # find groups of adjacent or almost adjacent v… joltages,
+    # which are the ones that "multiply" the number of possibilities
+    for i in range(1, len(joltages)):
+        if joltages[i] < joltages[i - 1] + 3:
+            new_group.append(joltages[i])
+        else:
+            groups.append(new_group)
+            new_group = []
+    groups.append(new_group)
+    groups = tuple(filter(lambda g: len(g) > 1, groups))
+
+    # groups of 2 adjacent numbers multiply the possibilities by 2
+    # groups of 3 adjacent numbers multiply the possibilities by 4
+    # groups of 4 adjacent numbers multiply the possibilities by 7
+    multipliers = {2: 2, 3: 4, 4: 7}
+
+    return math.prod(multipliers[length] for length in map(len, groups))
+
+
+u.assert_equals(find_all_combinations_of_adapters(second_example_adapters), 19208)
+u.answer_part_2(find_all_combinations_of_adapters(raw_input))
