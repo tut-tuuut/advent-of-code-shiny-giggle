ea95978cb6d0bc06cb5c4feaaf6b3a97596c5ea6
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-13 21:47:59 +0100

    Add grandiose and efficient way of calculating the outcome of a given fight
    You cannot have grandiose AND efficient, no

--- 2015/21/code.py
+++ 2015/21/code.py
@@ -73,19 +73,68 @@ class Bonhomme:
         GNON, pronounced "nion", is a very technical French term designing a punch, a slap, etc.
         (It is derived from the word "onion", apparently)
         """
-        self.hp -= attack - self.armor
+        self.hp -= max(0, attack - self.armor)
+        if self.hp < 0:
+            print(f"💥 OVERKILL!!! 💥 {self.avatar} = 💀".center(60))
+            self.hp = 0
 
     def display_status(self):
         draw_progress_bar(self.hp, self.max_hp, self.avatar)
+
+
+def grandiose_fight(player, boss):
+    """Display the fight between a player and a boss,
+    with a lot of useless but beautiful visual effects."""
+    round_count = 0
+    while player.hp > 0 and boss.hp > 0:
+        round_count += 1
+        print(f" ⚡️ ROUND {round_count} ! ⚡️".center(59, "-"))
+        boss.take_a_gnon(player.attack)
+        if boss.hp > 0:
+            player.take_a_gnon(boss.attack)
+        boss.display_status()
+        player.display_status()
+        print("")
+    if boss.hp <= 0:
+        print(f" {player.avatar} WINS! VICTORY ! ".center(40, "🌟"))
+    else:
+        print(f" {player.avatar} HAS LOST! DEFEAT! ".center(40, "💩"))
+    print("")
+
+
+def poutrage_index(player, boss):
+    """Poutrage index is >= 0 if the player wins, < 0 if the boss wins.
+    A higer absolute value indicates a more radiant victory or a more crushing defeat.
+    The French term for that is "poutrage", from the verb "poutrer" like in "j’ai poutré le boss",
+    or like in "le boss m’a poutré·e", referring to the fact you could as well have used some poutres
+    to win/lose the fight.
+    """
+    rounds_for_player_to_win = ceil(boss.hp / (player.attack - boss.armor))
+    rounds_for_boss_to_win = ceil(player.hp / (boss.attack - player.armor))
+    return rounds_for_boss_to_win - rounds_for_player_to_win
+
+
+def stuff_player(base_player_stats, stuff_stats):
+    return Bonhomme(
+        base_player_stats[0],
+        base_player_stats[1],
+        base_player_stats[2] + sum(stat[0] for stat in stuff_stats),
+        base_player_stats[3] + sum(stat[1] for stat in stuff_stats),
+    )
 
 
 # draw_progress_bar(60, 100, "😻")
 # draw_progress_bar(10, 80, "🧚‍")
 # draw_progress_bar(45, 120, "🐉")
 
-boss = Bonhomme("🐉", 109, 8, 2)
+# example_player = Bonhomme("😻", 8, 5, 5)
+# example_boss = Bonhomme("🧚‍", 12, 7, 2)
+# grandiose_fight(example_player, example_boss)
+# print(poutrage_index(example_player, example_boss))
 
-boss.take_a_gnon(12)
-boss.take_a_gnon(5)
+boss_stats = ("🐹", 109, 8, 2)
+player_stats = ("🎅", 100, 0, 0)
 
-boss.display_status()
+player_stuff = ((8, 0), (0, 2))
+
+grandiose_fight(stuff_player(player_stats, player_stuff), Bonhomme(*boss_stats))
