7c0bc88f370b39356cbb32ff24d483f792aed7b1
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-14 10:18:25 +0100

    2020 14 part 2 with a new strategy

--- 2020/14/code.py
+++ 2020/14/code.py
@@ -18,6 +18,24 @@ MEMORY_INSTRUCTION = re.compile(r"mem\[(\d+)\] = (\d+)")
 # part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
 
 
+def bits_from_integer(integer):
+    bits = list(bin(integer)[2:])
+    bits = ["0"] * (36 - len(bits)) + bits
+    return list(reversed(bits))
+
+
+def bits_to_integer(bits):
+    bits = list(reversed(bits))
+    return int("".join(bits), 2)
+
+
+u.assert_equals(bits_to_integer(bits_from_integer(25)), 25, "bits_from/to_integer")
+
+u.assert_equals(
+    bits_to_integer(bits_from_integer(2 ** 36)), 2 ** 36, "bits_from/to_integer"
+)
+
+
 def parse_input_to_program(raw_input):
     memory = defaultdict(lambda: 0)
     for line in raw_input.splitlines():
@@ -25,12 +43,11 @@ def parse_input_to_program(raw_input):
             mask = line[7:]
         elif line.startswith("mem"):
             address, value = tuple(map(int, re.findall(MEMORY_INSTRUCTION, line)[0]))
+            bits = bits_from_integer(value)
             for i, mask_val in enumerate(reversed(mask)):
-                if mask_val == "1":
-                    value = value | 2 ** i
-                elif mask_val == "0":
-                    value = value & (int("1" * 35, 2) - 2 ** i)
-            memory[address] = value
+                if mask_val != "X":
+                    bits[i] = mask_val
+            memory[address] = bits_to_integer(bits)
     return sum(memory.values())
 
 
@@ -51,23 +68,19 @@ def parse_input_to_program_v2(raw_input):
             address, target_value = tuple(
                 map(int, re.findall(MEMORY_INSTRUCTION, line)[0])
             )
+            bits = bits_from_integer(address)
             floating_bits = set()
             target_addresses = set()
-            for i, mask_val in enumerate(reversed(mask)):
+            for i, mask_val in enumerate(mask[::-1]):
                 if mask_val == "1":
-                    address = address | 2 ** i
+                    bits[i] = "1"
                 elif mask_val == "X":
                     floating_bits.add(i)
-            for values in itertools.product(*[[0, 1]] * len(floating_bits)):
-                target_address = address
+            for values in itertools.product(*[["0", "1"]] * len(floating_bits)):
+                target_bits = bits.copy()
                 for index, value in zip(floating_bits, values):
-                    if value == 1:
-                        target_address = target_address | 2 ** index
-                    elif value == 0:
-                        target_address = target_address & (
-                            int("1" * 35, 2) - 2 ** index
-                        )
-                target_addresses.add(target_address)
+                    target_bits[index] = value
+                target_addresses.add(bits_to_integer(target_bits))
             # print(
             #     f"writing {target_value} in {', '.join(map(str,sorted(target_addresses)))}"
             # )
@@ -86,3 +99,4 @@ u.answer_part_2(parse_input_to_program_v2(raw_input))
 
 # 3279437161092 not the right answer
 # 2851461851581 is too low
+# 3278997609887 right answer for part 2
