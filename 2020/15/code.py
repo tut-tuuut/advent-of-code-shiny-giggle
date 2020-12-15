01d9e6df654961d25b4329f3822cd17c3d1556bd
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-15 10:48:53 +0100

    2020 15 ⭐️ and ⭐️⭐️

--- 2020/15/code.py
+++ 2020/15/code.py
@@ -23,25 +23,28 @@ def find_nth_number_in_drinking_game(starting_numbers, n):
         {number: [rank] for rank, number in enumerate(starting_numbers, 1)}
     )
     last_spoken_number = starting_numbers[-1]
-    print(dict(memorization))
     for rank in range(len(starting_numbers) + 1, n + 1):
-        print("------")
-        print("rank", rank)
-        print(dict(memorization))
+        if rank % 1000 == 0:
+            print(rank, end="\r")
         if len(memorization[last_spoken_number]) >= 2:
-            print(
-                f"{last_spoken_number} was seen at range {memorization[last_spoken_number][-2]}"
-            )
             last_spoken_number = rank - 1 - memorization[last_spoken_number][-2]
         else:
-            print(f"{last_spoken_number} is new")
             last_spoken_number = 0
-        print("say", last_spoken_number)
         if rank == n:
             return last_spoken_number
+        # maybe for memory optimization we could shorten the list here,
+        # to keep only the 2 last indexes. But it worked for both parts
+        # without the optimization, so…
         memorization[last_spoken_number].append(rank)
 
 
 u.assert_equals(find_nth_number_in_drinking_game((0, 3, 6), 10), 0)
 
+for starting_numbers, expected in examples.items():
+    u.assert_equals(find_nth_number_in_drinking_game(starting_numbers, 2020), expected)
+
+u.answer_part_1(find_nth_number_in_drinking_game(my_input, 2020))
+
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+u.answer_part_2(find_nth_number_in_drinking_game(my_input, 30000000))
