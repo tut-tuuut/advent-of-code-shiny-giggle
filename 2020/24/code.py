3452f6948d2f480472adf75fe5a0eee54e8f474d
Agnès Haasser <agnes.haasser@gmail.com>
2020-12-24 17:02:45 +0100

    2020 24 ⭐️⭐️

--- 2020/24/code.py
+++ 2020/24/code.py
@@ -56,14 +56,90 @@ def extract_black_tiles(path_descriptor: str):
                 col += 1
             else:
                 print("duh?")
-        tiling[(row, col)] *= -1
+        tiling[row, col] *= -1
     return tiling
 
 
-tiling = extract_black_tiles(example)
-u.assert_equals(list(tiling.values()).count(-1), 10)
+def count_black_tiles(grid: dict):
+    return list(grid.values()).count(-1)
+
+
+example_tiling = extract_black_tiles(example)
+u.assert_equals(count_black_tiles(example_tiling), 10)
 
 tiling = extract_black_tiles(raw_input)
-u.answer_part_1(list(tiling.values()).count(-1))
+u.assert_equals(count_black_tiles(tiling), 317)
+
+u.answer_part_1(count_black_tiles(tiling))
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def count_black_neighbors(grid: defaultdict, coordinates: tuple):
+    return [
+        grid[coordinates] for coordinates in get_neighbors_coordinates(coordinates)
+    ].count(-1)
+
+
+def get_neighbors_coordinates(coordinates: tuple):
+    row, col = coordinates
+    neighbors_offsets = ((0, 2), (0, -2), (1, 1), (1, -1), (-1, -1), (-1, 1))
+    for row_offset, col_offset in neighbors_offsets:
+        yield row + row_offset, col + col_offset
+
+
+def build_next_grid(grid):
+    current_keys = set(grid.keys())
+    next_keys = current_keys.copy()
+    for tile in current_keys:
+        for neighbor in get_neighbors_coordinates(tile):
+            next_keys.add(neighbor)
+    new_grid = defaultdict(lambda: 1)
+    for key in next_keys:
+        if grid[key] == -1:
+            # it is black: turns white if zero or more than 2 black neighbors
+            # so it stays black if 1 or 2 black neighbors
+            if count_black_neighbors(grid, key) in (1, 2):
+                new_grid[key] = -1
+        elif grid[key] == 1:
+            if count_black_neighbors(grid, key) == 2:
+                new_grid[key] = -1
+    return new_grid
+
+
+tests = {
+    1: 15,
+    2: 12,
+    3: 25,
+    4: 14,
+    5: 23,
+    6: 28,
+    7: 41,
+    8: 37,
+    9: 49,
+    10: 37,
+    20: 132,
+    30: 259,
+    40: 406,
+    50: 566,
+    60: 788,
+    70: 1106,
+    80: 1373,
+    90: 1844,
+    100: 2208,
+}
+
+for days in range(1, 101):
+    example_tiling = build_next_grid(example_tiling)
+    if days in tests:
+        u.assert_equals(
+            count_black_tiles(example_tiling),
+            tests[days],
+            f" black tiles after {days} days",
+        )
+
+for days in range(100):
+    tiling = build_next_grid(tiling)
+
+u.answer_part_2(count_black_tiles(tiling))
+# 3822 too high because I did only 99 days DUH
