cf4dc922afba5c87e1abc79faff81e73fd8ee97d
Agnès Haasser <agnes@corelka.fr>
2021-12-01 10:49:47 +0100

    2021-01 use more_itertools.windowed instead of handcrafted windows

--- 2021/01/code.py
+++ 2021/01/code.py
@@ -1,4 +1,4 @@
-import itertools
+import more_itertools
 
 import utils as u
 
@@ -22,7 +22,7 @@ example_input = """199
 
 def count_increased_measurements(raw_input):
     ints = [int(row) for row in raw_input.splitlines()]
-    return sum(1 if ints[i + 1] > ints[i] else 0 for i in range(len(ints) - 1))
+    return sum(1 if w[1] > w[0] else 0 for w in more_itertools.windowed(ints, 2))
 
 
 u.assert_equals(count_increased_measurements(example_input), 7)
@@ -35,11 +35,8 @@ u.answer_part_1(count_increased_measurements(raw_input))  # 1400
 # len 6
 def count_increasing_windows(raw_input):
     ints = [int(row) for row in raw_input.splitlines()]
-    window_sums = [sum(ints[i : i + 2]) for i in range(len(ints) - 1)]
-    return sum(
-        1 if window_sums[i + 1] > window_sums[i] else 0
-        for i in range(len(window_sums) - 1)
-    )
+    window_sums = [sum(w) for w in more_itertools.windowed(ints, 3)]
+    return sum(1 if s[1] > s[0] else 0 for s in more_itertools.windowed(window_sums, 2))
 
 
 u.assert_equals(count_increasing_windows(example_input), 5)
