629653ad16b9f3aad6b8d6254c01384c1568102a
Agnès Haasser <agnes@corelka.fr>
2021-12-07 13:46:38 +0100

    2021-07 *

---
+++ 2021/07/code.py
@@ -0,0 +1,29 @@
+import utils as u
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+example_input = "16,1,2,0,4,2,7,1,2,14"
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def fuel_consumed_for_position(target, positions):
+    return sum(abs(target - x) for x in positions)
+
+
+def where_should_we_align_the_crabs(raw_input):
+    positions = tuple(map(int, raw_input.split(",")))
+    min_consumed_fuel = 5 * sum(positions)
+    optimal_position = None
+    for x in range(min(positions), max(positions)):
+        fuel = fuel_consumed_for_position(x, positions)
+        if fuel < min_consumed_fuel:
+            min_consumed_fuel = fuel
+            optimal_position = x
+    return optimal_position, min_consumed_fuel
+
+
+u.assert_equals(where_should_we_align_the_crabs(example_input), (2, 37))
+u.answer_part_1(where_should_we_align_the_crabs(raw_input)[1])
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
