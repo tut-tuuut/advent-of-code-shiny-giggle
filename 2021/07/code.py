ca6255e3167572da907cbfc18200a5769c94aed2
Agnès Haasser <agnes@corelka.fr>
2021-12-07 13:55:33 +0100

    2021-07 ** wrong and slow

--- 2021/07/code.py
+++ 2021/07/code.py
@@ -27,3 +27,23 @@ u.assert_equals(where_should_we_align_the_crabs(example_input), (2, 37))
 u.answer_part_1(where_should_we_align_the_crabs(raw_input)[1])
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+def fuel_consumed_for_position_part2(target, positions):
+    return sum(sum(range(1+abs(target - x))) for x in positions)
+
+
+def where_should_we_align_the_crabs_part2(raw_input):
+    positions = tuple(map(int, raw_input.split(",")))
+    min_consumed_fuel = 5 * sum(positions)
+    optimal_position = None
+    for x in range(min(positions), max(positions)):
+        fuel = fuel_consumed_for_position_part2(x, positions)
+        if fuel < min_consumed_fuel:
+            min_consumed_fuel = fuel
+            optimal_position = x
+    return optimal_position, min_consumed_fuel
+
+u.assert_equals(where_should_we_align_the_crabs_part2(example_input), (5, 168))
+u.answer_part_2(where_should_we_align_the_crabs_part2(raw_input)[1])
+
+# 2332855 too low
