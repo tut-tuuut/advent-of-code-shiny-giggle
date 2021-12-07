57346cccb5579f0c12aad4a9c8184b61ffaa74f3
Agnès Haasser <agnes@corelka.fr>
2021-12-07 14:01:00 +0100

    2017 **

--- 2021/07/code.py
+++ 2021/07/code.py
@@ -28,20 +28,25 @@ u.answer_part_1(where_should_we_align_the_crabs(raw_input)[1])
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
 
+
 def fuel_consumed_for_position_part2(target, positions):
-    return sum(sum(range(1+abs(target - x))) for x in positions)
+    # maths: sum(1,2....,N) = N*(N+1)/2
+    return sum(
+        int(abs(target - crab) * (1 + abs(target - crab)) / 2) for crab in positions
+    )
 
 
 def where_should_we_align_the_crabs_part2(raw_input):
     positions = tuple(map(int, raw_input.split(",")))
-    min_consumed_fuel = 5 * sum(positions)
+    min_consumed_fuel = None
     optimal_position = None
     for x in range(min(positions), max(positions)):
         fuel = fuel_consumed_for_position_part2(x, positions)
-        if fuel < min_consumed_fuel:
+        if min_consumed_fuel is None or fuel < min_consumed_fuel:
             min_consumed_fuel = fuel
             optimal_position = x
     return optimal_position, min_consumed_fuel
+
 
 u.assert_equals(where_should_we_align_the_crabs_part2(example_input), (5, 168))
 u.answer_part_2(where_should_we_align_the_crabs_part2(raw_input)[1])
