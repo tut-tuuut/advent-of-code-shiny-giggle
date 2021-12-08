aa93623fd7a12bc3e8af76abac96203918a7e4b9
Agnès Haasser <agnes@corelka.fr>
2021-12-08 18:09:33 +0100

    2021-0! *

---
+++ 2021/08/code.py
@@ -0,0 +1,30 @@
+import utils as u
+import itertools
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+example_input = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def count_easy_letters_in_output(raw_input):
+    outputs = [row.split(" | ")[1] for row in raw_input.splitlines()]
+    outputs = tuple(itertools.chain(*(r.split() for r in outputs)))
+    return len(tuple(filter(lambda x: len(x) in (2, 4, 3, 7), outputs)))
+
+
+u.assert_equals(count_easy_letters_in_output(example_input), 26)
+u.answer_part_1(count_easy_letters_in_output(raw_input))
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
