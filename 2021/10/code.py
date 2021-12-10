2661fea6e0528b008cf86f2ad20de5250371789f
Agnès Haasser <agnes@corelka.fr>
2021-12-10 14:03:35 +0100

    2021-10 **

--- 2021/10/code.py
+++ 2021/10/code.py
@@ -1,4 +1,5 @@
 import utils as u
+import statistics
 
 with open(__file__ + ".input.txt", "r+") as file:
     raw_input = file.read()
@@ -48,3 +49,31 @@ u.assert_equals(find_illegal_characters(example_input), 26397)
 u.answer_part_1(find_illegal_characters(raw_input))
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def find_closing_sequences(raw_input):
+    cleaned_input = clean_string(raw_input)
+    illegals = "]>})"
+    legal_rows_reversed = [
+        row[::-1]
+        for row in cleaned_input.splitlines()
+        if not any((i in row) for i in illegals)
+    ]
+    scores = {
+        "(": 1,
+        "[": 2,
+        "{": 3,
+        "<": 4,
+    }
+    all_row_scores = []
+    for row in legal_rows_reversed:
+        row_score = 0
+        for c in row:
+            row_score *= 5
+            row_score += scores[c]
+        all_row_scores.append(row_score)
+    return statistics.median(all_row_scores)
+
+
+u.assert_equals(find_closing_sequences(example_input), 288957)
+u.answer_part_2(find_closing_sequences(raw_input))
