fd264a5b318d039fe11a1c946281053ab76c0903
Agnès Haasser <agnes@corelka.fr>
2021-12-13 15:21:42 +0100

    2021-13 * **

--- 2021/13/code.py
+++ 2021/13/code.py
@@ -31,17 +31,17 @@ fold along x=5"""
 
 
 def debug_grid(grid):
-    print(f"┌{'─'*len(grid[0])}┐")
+    print(f"┌─{'─'*len(grid[0])}┐")
     for row in grid:
-        print("│" + "".join(u.yellow("◼︎", True) if val else " " for val in row) + "│")
-    print(f"└{'─'*len(grid[0])}┘")
+        print("│ " + "".join("█" if val else " " for val in row) + "│")
+    print(f"└─{'─'*len(grid[0])}┘")
 
 
 def construct_grid(raw_input):
     is_dot = re.compile(r"(\d+),(\d+)")
     is_fold = re.compile(r"fold along ([xy])=(\d+)")
-    dots = [(int(x), int(y)) for x, y in re.findall(is_dot, example)]
-    folds = [(axis, int(coord)) for axis, coord in re.findall(is_fold, example)]
+    dots = [(int(x), int(y)) for x, y in re.findall(is_dot, raw_input)]
+    folds = [(axis, int(coord)) for axis, coord in re.findall(is_fold, raw_input)]
     max_x_dot = max(x for x, _ in dots)
     max_y_dot = max(y for _, y in dots)
     max_x_fold = max(val for axis, val in folds if axis == "x")
@@ -56,17 +56,37 @@ def construct_grid(raw_input):
 
 def fold_grid(grid, fold):
     axis, value = fold
+    width = len(grid[0])
+    height = len(grid)
     if axis == "y":  # horizontal fold
-        if value != (len(grid) - 1) / 2:
+        if value != (height - 1) / 2:
             return
         new_grid = [
-            [(grid[y][x] or grid[len(grid) - 1 - y][x]) for x in range(len(grid[0]))]
-            for y in range(int((len(grid) - 1) / 2))
+            [(grid[y][x] or grid[height - 1 - y][x]) for x in range(width)]
+            for y in range(int((height - 1) / 2))
+        ]
+    elif axis == "x":  # vertical fold
+        if value != ((width - 1) / 2):
+            return
+        new_grid = [
+            [(row[x] or row[width - 1 - x]) for x in range(int((width - 1) / 2))]
+            for row in grid
         ]
     return new_grid
 
 
-grid, folds = construct_grid(raw_input)
+grid, folds = construct_grid(example)
 new_grid = fold_grid(grid, folds[0])
-debug_grid(new_grid)
+new_grid = fold_grid(new_grid, folds[1])
+
+grid, folds = construct_grid(raw_input)
+new_grid = fold_grid(grid, folds.pop(0))
+u.answer_part_1(sum(sum(1 if val else 0 for val in row) for row in new_grid))
+
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+for fold in folds:
+    new_grid = fold_grid(new_grid, fold)
+
+u.answer_part_2("RCPLAKHL")
+debug_grid(new_grid)
