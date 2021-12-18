66b2c97133851178ad3b8c3f85426396f07e33b4
Agnès Haasser <agnes@corelka.fr>
2021-12-18 12:07:53 +0100

    2021-18 WIP on explode()
    Look at "infixe" for binary tree

--- 2021/18/code.py
+++ 2021/18/code.py
@@ -1,4 +1,5 @@
 import re
+from math import floor, ceil
 import utils as u
 
 
@@ -6,6 +7,7 @@ class SnailfishNb:
     def __init__(self, description, level=0, parent=None):
         self.level = level
         self.parent = parent
+        self.exploded = False
         if re.match(r"\[\d+,\d+\]", description):
             self.left, self.right = map(int, description[1:-1].split(","))
             return
@@ -48,6 +50,56 @@ class SnailfishNb:
         if isinstance(self.right, self.__class__):
             self.right.increase_level()
 
+    def is_two_regulars(self):
+        return isinstance(self.left, int) and isinstance(self.right, int)
+
+    def is_regular_left(self):
+        return isinstance(self.left, int)
+
+    def is_regular_right(self):
+        return isinstance(self.right, int)
+
+    def reduce(self):
+        if self.is_two_regulars() and self.level >= 4:
+            self.explode()
+            return True
+        if self.is_regular_left():
+            if self.left >= 10:
+                description = f"[{floor(self.left/2)},{ceil(self.left/2)}]"
+                self.left = SnailfishNb(description, level=self.level + 1, parent=self)
+                return True
+        if self.is_regular_right():
+            if self.right >= 10:
+                description = f"[{floor(self.right/2)},{ceil(self.right/2)}]"
+                self.right = SnailfishNb(description, level=self.level + 1, parent=self)
+                return True
+        if not self.is_regular_left() and self.left.reduce():
+            if self.left.exploded:
+                self.left = 0
+            return True
+        if not self.is_regular_right() and self.right.reduce():
+            if self.right.exploded:
+                self.right = 0
+            return True
+        return False
+
+    def explode(self):
+        self.parent.add_on_left(self.left)
+        self.parent.add_on_right(self.right)
+        self.exploded = True
+
+    def add_on_left(self, value):
+        if self.is_regular_left():
+            self.left += value
+        elif self.parent is not None:
+            self.parent.add_on_left(value)
+
+    def add_on_right(self, value):
+        if self.is_regular_right():
+            self.right += value
+        elif self.parent is not None:
+            self.parent.add_on_right(value)
+
 
 examples = """[1,2]
 [[1,2],3]
@@ -74,6 +126,21 @@ u.assert_equals(result.left.level, 1)
 u.assert_equals(result.right.parent, result)
 u.assert_equals(result.right.level, 1)
 u.assert_equals(result.right.left.level, 2)
+
+u.pink("-'*'-.,__,.-'*'-( explode tests )-'*'-.,__,.-'*'-")
+
+explode_examples = (
+    ("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"),
+    ("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]"),
+    ("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"),
+    ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"),
+    ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"),
+)
+
+for before, after in explode_examples:
+    nb = SnailfishNb(before)
+    nb.reduce()
+    u.assert_equals(str(nb), after)
 
 # part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
 
