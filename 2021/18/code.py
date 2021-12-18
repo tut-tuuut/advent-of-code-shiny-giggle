d033bbf316a27a60e2c97a201f4fcd71e3708722
Agnès Haasser <agnes@corelka.fr>
2021-12-18 10:30:25 +0100

    2021-18 parse snailfish numbers

--- 2021/18/code.py
+++ 2021/18/code.py
@@ -1,4 +1,44 @@
+import re
 import utils as u
+
+
+class SnailfishNb:
+    def __init__(self, description, level=0, parent=None):
+        self.level = level
+        self.parent = parent
+        if re.match(r"\[\d+,\d+\]", description):
+            self.left, self.right = map(int, description[1:-1].split(","))
+            return
+        description = description[1:-1]
+        for comma in re.finditer(",", description):
+            half_string = description[comma.start() :]
+            if half_string.count("]") == half_string.count("["):
+                left_string = description[: comma.start()]
+                right_string = description[comma.end() :]
+                if left_string.isnumeric():
+                    self.left = int(left_string)
+                else:
+                    self.left = SnailfishNb(
+                        left_string, level=self.level + 1, parent=self
+                    )
+                if right_string.isnumeric():
+                    self.right = int(right_string)
+                else:
+                    self.right = SnailfishNb(
+                        right_string, level=self.level + 1, parent=self
+                    )
+
+
+examples = """[1,2]
+[[1,2],3]
+[9,[8,7]]
+[[1,9],[8,5]]
+[[[[1,2],[3,4]],[[5,6],[7,8]]],9]
+[[[9,[3,8]],[[0,9],6]],[[[3,7],[4,9]],3]]
+[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"""
+
+for x in examples.splitlines():
+    SnailfishNb(x)
 
 with open(__file__ + ".input.txt", "r+") as file:
     raw_input = file.read()
