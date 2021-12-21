af59753cf1697b7a9fa42709b5e74879a07f5c8e
Agnès Haasser <agnes@corelka.fr>
2021-12-21 15:23:45 +0100

    2021-21 ** WIP

--- 2021/21/code.py
+++ 2021/21/code.py
@@ -1,4 +1,5 @@
 import utils as u
+from collections import deque
 
 with open(__file__ + ".input.txt", "r+") as file:
     raw_input = file.read()
@@ -29,3 +30,46 @@ u.assert_equals(part_1(4, 8), 739785)
 u.answer_part_1(part_1(8, 7))
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def debug_explored(dict_explored):
+    for dice, status in dict_explored.items():
+        u.yellow("-".join(str(d) for d in dice))
+        positions, scores = status
+        print(f"positions :", positions)
+        print(f"scores :", scores)
+
+
+def part_2(starting_position_one, starting_position_two):
+    positions = [starting_position_one, starting_position_two]
+    explored = {(): ((starting_position_one, starting_position_two), (0, 0))}
+    to_explore = deque([(1,), (2,), (3,)])
+    winning_universes = [0, 0]
+    while len(to_explore):
+        to_try = to_explore.popleft()
+        die_value = to_try[-1]
+        positions, scores = explored[to_try[:-1]]
+        player = ((len(to_try) - 1) // 3) % 2
+        new_positions = list(positions)
+        new_positions[player] = (new_positions[player] + die_value) % 10
+        if len(to_try) % 3 == 0:
+            new_scores = list(scores)
+            new_scores[player] += new_positions[player]
+            if new_positions[player] == 0:
+                new_scores[player] += 10
+        else:
+            new_scores = scores
+        if max(new_scores) < 21:
+            explored[to_try] = (tuple(new_positions), tuple(new_scores))
+            for new_dice_value in (1, 2, 3):
+                to_explore.appendleft(to_try + (new_dice_value,))
+        else:
+            if new_scores[0] >= 21:
+                winning_universes[0] += 1
+            elif new_scores[1] >= 21:
+                winning_universes[1] += 1
+            print(sum(winning_universes), end="\r")
+    return max(winning_universes)
+
+
+u.assert_equals(part_2(4, 8), 444356092776315)
