a90bc052c1d88ff7192b1c9134193bbe7ea2821d
Agnès Haasser <agnes@corelka.fr>
2021-12-21 22:59:08 +0100

    2021-21 **

--- 2021/21/code.py
+++ 2021/21/code.py
@@ -47,38 +47,40 @@ u.assert_equals(sum(frequencies.values()), 3 * 3 * 3)
 
 
 @cache
-def get_positions_and_scores(dice_history, starting_positions):
-    if len(dice_history) == 0:
-        return starting_positions, (0, 0)
-    positions, scores = get_positions_and_scores(dice_history[:-1], starting_positions)
-    die_value = dice_history[-1]
-    player = (len(dice_history) - 1) % 2
-    new_positions = list(positions)
-    new_positions[player] = (new_positions[player] + die_value) % 10
-    new_scores = list(scores)
-    new_scores[player] += new_positions[player]
-    if new_positions[player] == 0:
-        new_scores[player] += 10
-    return tuple(new_positions), tuple(new_scores)
+def get_winning_outcomes(p1, p2, s1, s2, player):
+    w1 = w2 = 0
+    if player == 1:
+        for dice_result, frequency in frequencies.items():
+            new_p1 = (p1 + dice_result) % 10
+            new_s1 = s1 + new_p1
+            if new_p1 == 0:
+                new_s1 += 10
+            if new_s1 >= 21:
+                w1 += frequency
+            else:
+                plus_w1, plus_w2 = get_winning_outcomes(new_p1, p2, new_s1, s2, 2)
+                w1 += plus_w1 * frequency
+                w2 += plus_w2 * frequency
+    elif player == 2:
+        for dice_result, frequency in frequencies.items():
+            new_p2 = (p2 + dice_result) % 10
+            new_s2 = s2 + new_p2
+            if new_p2 == 0:
+                new_s2 += 10
+            if new_s2 >= 21:
+                w2 += frequency
+            else:
+                plus_w1, plus_w2 = get_winning_outcomes(p1, new_p2, s1, new_s2, 1)
+                w1 += plus_w1 * frequency
+                w2 += plus_w2 * frequency
+    return w1, w2
 
 
 def part_2(starting_position_one, starting_position_two):
-    starting_positions = (starting_position_one, starting_position_two)
-    to_explore = deque((value,) for value in frequencies.keys())
-    winning_universes = [0, 0]
-    while len(to_explore) > 0:
-        to_try = to_explore.popleft()
-        _, scores = get_positions_and_scores(to_try, starting_positions)
-        if max(scores) < 21:
-            for new_dice_value in frequencies.keys():
-                to_explore.appendleft(to_try + (new_dice_value,))
-        else:
-            if scores[0] >= 21:
-                winning_universes[0] += frequencies[to_try[-1]]
-            elif scores[1] >= 21:
-                winning_universes[1] += frequencies[to_try[-1]]
-            print(sum(winning_universes), end="\r")
-    return max(winning_universes)
+    return max(
+        get_winning_outcomes(starting_position_one, starting_position_two, 0, 0, 1)
+    )
 
 
-u.assert_equals(part_2(4, 8), 444356092776315)
+u.assert_equals(get_winning_outcomes(4, 8, 0, 0, 1), (444356092776315, 341960390180808))
+u.answer_part_2(part_2(8, 7))
