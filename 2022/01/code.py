c12f6fc71f7c024ee3d44cb979fbeb57de0d722f
Agnès Haasser <agnes@corelka.fr>
2022-12-10 22:53:47 +0100

    Go AOC 2022!

---
+++ 2022/01/code.py
@@ -0,0 +1,11 @@
+import utils as u
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+u.assert_equals("yey", "yey", "")
+u.answer_part_1('test bonjour')
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
