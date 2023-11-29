7efb4e87c49ad6d90566d715208d19c0eca5d178
Agnès Haasser <agnes.haasser@beta.gouv.fr>
2023-11-29 14:11:10 +0100

    2022-03 A

--- 2022/03/code.py
+++ 2022/03/code.py
@@ -1,7 +1,7 @@
 import utils
 
-#with open(__file__ + ".input.txt", "r+") as file:
-#    raw_input = file.read()
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
 
 example_input = """vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -13,14 +13,36 @@ CrZsJsPPZsGzwwsLwLmpwMDw
 
 # part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
 
+
 def find_item_in_both_compartments(rucksack_string):
-    if len(rucksack_string)%2 != 0:
+    if len(rucksack_string) % 2 != 0:
         raise Exception("I need an even-length string")
-    compartment_size = int(len(rucksack_string)/2)
+    compartment_size = int(len(rucksack_string) / 2)
     first_compartment = set(rucksack_string[:compartment_size])
     second_compartment = set(rucksack_string[compartment_size:])
     return (first_compartment & second_compartment).pop()
 
-utils.assert_equals(find_item_in_both_compartments("abcdaf"),"a")
+
+utils.assert_equals(find_item_in_both_compartments("abcdaf"), "a")
+
+
+priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def priority(letter):
+    return 1 + priorities.index(letter)
+
+
+utils.assert_equals(priority("a"), 1)
+utils.assert_equals(priority("Z"), 52)
+
+utils.assert_equals(
+    sum(priority(find_item_in_both_compartments(row)) for row in example_input.split()),
+    157,
+)
+
+utils.answer_part_1(
+    sum(priority(find_item_in_both_compartments(row)) for row in raw_input.split())
+)
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
