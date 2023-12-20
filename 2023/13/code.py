5447ac4831a4f4824232863f55bccefc675ffe8e
Agnès Haasser <agnes@corelka.fr>
2023-12-20 14:24:32 +0100

    202320A

--- 2023/13/code.py
+++ 2023/13/code.py
@@ -10,6 +10,22 @@ grids = raw_input.split("\n\n")
 
 def find_reflection(grid, debug=False):
     rows = grid.strip().split("\n")
+    horiz = find_horiz_reflection(rows)
+    if debug:
+        print("\n".join("".join(row) for row in rows))
+        print("\n\n")
+
+    if horiz:
+        return horiz
+    transposed_rows = []
+    for j in range(len(rows[0])):
+        transposed_rows.append(tuple(row[j] for row in rows))
+    if debug:
+        print("\n".join("".join(row) for row in transposed_rows))
+    return find_horiz_reflection(transposed_rows, factor=1)
+
+
+def find_horiz_reflection(rows, debug=False, factor=100):
     height = len(rows)
     if debug:
         print("height : ", height)
@@ -31,20 +47,7 @@ def find_reflection(grid, debug=False):
         ):
             if debug:
                 print(i, "is a symmetry")
-            return (i + 1) * 100
-
-
-find_reflection(
-    """
-#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#.
-"""
-)
+            return (i + 1) * factor
 
 
 u.assert_equal(
@@ -59,5 +62,29 @@ u.assert_equal(
     ),
     400,
 )
+
+
+u.assert_equal(
+    find_reflection(
+        """
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+"""
+    ),
+    5,
+)
+
+
+def part_1(raw_input):
+    grids = raw_input.strip().split("\n\n")
+    return sum(find_reflection(grid) for grid in grids)
+
+
+u.answer_part_1(part_1(raw_input))
 
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
