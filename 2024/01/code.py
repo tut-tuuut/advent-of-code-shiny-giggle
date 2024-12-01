a0c76296a6a5c2c7e3a5cc90bdd10c366c807b61
Agnès Haasser <agnes@hashbang.coop>
2024-12-01 11:52:01 +0100

    2024-01 *

---
+++ 2024/01/code.py
@@ -0,0 +1,36 @@
+import utils as u
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+example = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+
+def extract_lists_from_raw_input(raw_input):
+    llist = []
+    rlist  = []
+    for row in raw_input.splitlines():
+        if not row:
+            continue
+        left, right = row.split("   ")
+        llist.append(int(left))
+        rlist.append(int(right))
+    return llist, rlist
+
+def distance_between_two_lists(raw_input):
+    left, right = extract_lists_from_raw_input(raw_input)
+    left = sorted(left)
+    right = sorted(right)
+    return sum(abs(l-r) for l, r in zip(left, right))
+
+u.assert_equal(distance_between_two_lists(example), 11)
+
+u.answer_part_1(distance_between_two_lists(raw_input))
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
