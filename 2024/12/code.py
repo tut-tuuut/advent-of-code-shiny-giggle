d49008e1121bc50d354ad7f0e256f738bf9be3eb
Agnès Haasser <agnes@hashbang.coop>
2024-12-12 16:28:25 +0100

    2024-12 begin

---
+++ 2024/12/code.py
@@ -0,0 +1,32 @@
+import utils as u
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+ex140 = """AAAA
+BBCD
+BBCC
+EEEC"""
+
+ex772 = """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"""
+
+ex1930 = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+
+u.answer_part_1("allez")
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
