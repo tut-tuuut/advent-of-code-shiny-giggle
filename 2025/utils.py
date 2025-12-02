562d53a331e5bf769363ac4c4011cb67d7c0e69c
Agnès Haasser <agnes@hashbang.coop>
2025-12-02 11:19:48 +0100

    Init 2025

---
+++ 2025/utils.py
@@ -0,0 +1,72 @@
+import time
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+PURPLE = "\033[35m"
+CYAN = "\033[36m"
+PINK = "\033[38;5;206m"
+
+NORMAL = "\033[0m"
+
+
+def red(message):
+    print(f"{RED}{message}{NORMAL}")
+
+
+def green(message):
+    print(f"{GREEN}{message}{NORMAL}")
+
+
+def yellow(message, ret=False):
+    if ret:
+        return f"{YELLOW}{message}{NORMAL}"
+    print(f"{YELLOW}{message}{NORMAL}")
+
+
+def purple(message, ret=False):
+    if ret:
+        return f"{PURPLE}{message}{NORMAL}"
+    print(f"{PURPLE}{message}{NORMAL}")
+
+
+def blue(message, ret=False):
+    if ret:
+        return f"{CYAN}{message}{NORMAL}"
+    print(f"{CYAN}{message}{NORMAL}")
+
+
+def pink(message):
+    print(f"{PINK}{message}{NORMAL}")
+
+
+def assert_equal(tested, expected, comment=""):
+    if tested == expected:
+        print(f"{GREEN}(YAY){NORMAL} {tested} {comment}")
+    else:
+        print(f"{RED}(NAY){NORMAL} got {tested}, expected {expected}! {comment}")
+
+
+def answer_part_1(content):
+    print(f"{YELLOW}[PART 1 🎄] {content}{NORMAL}")
+
+
+def answer_part_2(content):
+    print(f"{YELLOW}[PART 2 🌟] {content}{NORMAL}")
+
+
+def nanotime():
+    return time.clock_gettime_ns(time.CLOCK_PROCESS_CPUTIME_ID)
+
+
+if __name__ == "__main__":
+    purple("violet !")
+    blue("bleu !")
+    green("vert !")
+    yellow("jaune !")
+    red("rouge !")
+    pink("rose !")
+    answer_part_1("réponse partie 1")
+    answer_part_2("réponse partie 2")
+    assert_equal("réel", "attendu", "test pour voir un test échouer")
+    assert_equal("youpi", "youpi", "pour voir quand un test passe")
