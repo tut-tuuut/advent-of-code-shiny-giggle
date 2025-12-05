c59518ad76d3aaef598841556605e8a443cb8b77
Agnès Haasser <agnes@hashbang.coop>
2025-12-05 19:43:35 +0100

    2025-03 part 1

---
+++ 2025/03/code.py
@@ -0,0 +1,36 @@
+import utils as u
+
+with open(__file__ + ".input.txt", "r+") as file:
+    raw_input = file.read()
+
+# part 1 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+example = """987654321111111
+811111111111119
+234234234234278
+818181911112111"""
+
+def joltage(given_str):
+    for first_digit in (reversed(range(1,10))):
+        idx = given_str[:-1].find(str(first_digit))
+        if idx >= 0:
+            for second_digit in (reversed(range(1,10))):
+                second_idx = given_str[idx+1:].find(str(second_digit))
+                if second_idx >= 0:
+                    return 10*first_digit + second_digit
+
+
+u.assert_equal(joltage("987654321111111"), 98)
+u.assert_equal(joltage("811111111111119"), 89)
+u.assert_equal(joltage("234234234234278"), 78)
+u.assert_equal(joltage("818181911112111"), 92)
+
+def part_1(given_input):
+    return sum(joltage(row) for row in given_input.splitlines())
+
+u.assert_equal(part_1(example), 357)
+
+u.answer_part_1(part_1(raw_input))
+
+# part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
