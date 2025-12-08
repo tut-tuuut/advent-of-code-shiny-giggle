1f30e685e79018fb6c6f23f0bacd12ec49e51b76
Agnès Haasser <agnes@hashbang.coop>
2025-12-08 08:48:44 +0100

    2025-06

--- 2025/06/code.py
+++ 2025/06/code.py
@@ -1,5 +1,6 @@
 import utils as u
 import numpy as np
+from math import prod
 
 with open(__file__ + ".input.txt", "r+") as file:
     raw_input = file.read()
@@ -11,8 +12,6 @@ example = """123 328  51 64
   6 98  215 314
 *   +   *   +  """
 
-u.answer_part_1("allez")
-
 
 def part_1(given_input):
     given_input_as_tuple = tuple(given_input.splitlines())
@@ -22,8 +21,51 @@ def part_1(given_input):
             for row in given_input_as_tuple[:-1]
         )
     )
-    print(arr)
+    numbers = arr.transpose()
+    operators = given_input_as_tuple[-1].split()
+    return sum(
+        (sum(operands) if (operator == "+") else prod(operands))
+        for operator, operands in zip(operators, numbers)
+    )
 
 
-part_1(example)
+u.assert_equal(part_1(example), 4277556)
+
+u.answer_part_1(part_1(raw_input))
+
+
 # part 2 -'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,__,.-'*'-.,_
+
+
+def yield_numbers_from_sheet(sheet):
+    stocked_numbers = []
+    for row in sheet:
+        if row == "":
+            yield stocked_numbers
+            stocked_numbers = []
+        else:
+            stocked_numbers.append(int(row))
+    yield stocked_numbers
+
+
+def part_2(given_input):
+    given_input_as_tuple = tuple(given_input.splitlines())
+    arr = np.array(
+        tuple(
+            tuple(int(cell) if cell.isdigit() else "" for cell in row)
+            for row in given_input_as_tuple[:-1]
+        )
+    )
+    numbers_as_str = list("".join(row) for row in arr.transpose())
+    operators = given_input_as_tuple[-1].split()
+    return sum(
+        (sum(operands) if (operator == "+") else prod(operands))
+        for operator, operands in zip(
+            operators, yield_numbers_from_sheet(numbers_as_str)
+        )
+    )
+
+
+u.assert_equal(part_2(example), 3263827)
+
+u.answer_part_2(part_2(raw_input))
